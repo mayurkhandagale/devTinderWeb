9c808bb5001b8189272c367f09195950d00f35a6
mayur <khandagalemayur2525@gmail.com>
2025-02-25 23:07:49 +0530

    Update profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux';
+import EditProfile from './EditProfile';
+
 const Profile = () => {
+  const user = useSelector((store) => store.user);
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <EditProfile user={user} />
+    </div>
+  );
+};
 
 export default Profile;
