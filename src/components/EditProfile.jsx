20a05422c0688189ababe9f071a4c95843653cdd
mayur <khandagalemayur2525@gmail.com>
2025-02-26 11:19:32 +0530

    feat: Connection page

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -7,12 +7,12 @@ import axios from "axios";
 
 const EditProfile = ({user}) => {
   console.log("user", user);
-  const [firstName, setFirtName] = useState(user.firstName); 
-  const [lastName, setLastName] = useState(user.lastName);
-  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
-  const [age, setAge] = useState(user.age); 
-  const [gender, setGender] = useState(user.gender);
-  const [about, setAbout] = useState(user.about);
+  const [firstName, setFirtName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [photoUrl, setPhotoUrl] = useState(user?.photoUrl);
+  const [age, setAge] = useState(user?.age);
+  const [gender, setGender] = useState(user?.gender);
+  const [about, setAbout] = useState(user?.about);
   const [showToast, setShowToast] = useState(false);
 
   const [error, setError] = useState("");
